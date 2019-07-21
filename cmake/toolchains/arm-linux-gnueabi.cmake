2c2d1974176c8ef4797d36b4e4abfe431ce79f5a
Victor Lee <lzs_1993@qq.com>
2019-07-21 21:41:09 +0800

    编译工程改造

---
+++ cmake/toolchains/arm-linux-gnueabi.cmake
@@ -0,0 +1,34 @@
+include(CMakeForceCompiler)
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_CROSSCOMPILING TRUE)
+
+#不使用动态链接 -rdyamic
+set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
+
+# The toolchain prefix for all toolchain executables
+set(CROSS_COMPILE arm-linux-gnueabi-)
+
+if (WIN32)
+    message(STATUS "Now is windows")
+elseif (UNIX)
+    message(STATUS "Now is linux")
+    set(CROSS_COMPILE_ROOT_PATH "/usr")
+    set(CMAKE_C_COMPILER    ${CROSS_COMPILE_ROOT_PATH}/bin/${CROSS_COMPILE}gcc)
+    set(CMAKE_CXX_COMPILER  ${CROSS_COMPILE_ROOT_PATH}/bin/${CROSS_COMPILE}g++)
+    set(CMAKE_ASM_COMPILER  ${CROSS_COMPILE_ROOT_PATH}/bin/${CROSS_COMPILE}as)
+    set(CMAKE_AR            ${CROSS_COMPILE_ROOT_PATH}/bin/${CROSS_COMPILE}gcc)
+    set(CMAKE_RANLIB        ${CROSS_COMPILE_ROOT_PATH}/bin/${CROSS_COMPILE}ranlib)
+    set(COVERAGE_COMMAND    ${CROSS_COMPILE_ROOT_PATH}/bin/${CROSS_COMPILE}gcov)
+    set(CMAKE_OBJCOPY       ${CROSS_COMPILE_ROOT_PATH}/bin/${CROSS_COMPILE}objcopy)
+    # set(CMAKE_MAKE_PROGRAM  ${CROSS_COMPILE_ROOT_PATH}/bin/make)
+endif ()
+
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,-gc-sections -mcpu=arm920t -static -mfloat-abi=soft")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_FIND_ROOT_PATH ${CROSS_COMPILE_ROOT_PATH})
